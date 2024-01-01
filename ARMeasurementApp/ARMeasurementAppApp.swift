0e1ed4261ca88564bdc87331cc8b233e487edbd0
Ishaan Goel <goelishaan2007@gmail.com>
2024-01-01 15:15:35 -0800

    Initial Commit

---
+++ ARMeasurementApp/ARMeasurementAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ARMeasurementAppApp.swift
+//  ARMeasurementApp
+//
+//  Created by Ishaan Goel on 1/1/24.
+//
+
+import SwiftUI
+
+@main
+struct ARMeasurementAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
